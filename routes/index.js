83aeda74b700537e1a005f6938487e0a48f8ec08
Yunus Emre <yek.plus.plus@gmail.com>
2019-10-06 23:39:57 +0300

    added main route

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const mainRouter require( './main');
+export const initializeRoutes = (app) => {
+  app.use('/main', mainRouter);
+};
+export const initializeError = (app) => {
+  app.use((req, res) => {
+    res.send(404, { error: 'not found' });
+  });
+};
+export default { initializeRoutes, initializeError };
