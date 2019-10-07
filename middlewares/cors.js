14801acdc5bcd78b0cacaeee9210f3a342503d39
Yunus Emre <yek.plus.plus@gmail.com>
2019-10-07 20:46:14 +0300

    update cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,6 @@
 module.exports = {
   origin(origin, callback) {
-    if (settings.whitelist.indexOf(origin) !== -1) {
+    if (settings.cors.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(null, true);
