3fe0b6608f5337054abb2386b02be48cc4ceae42
Yunus Emre <yek.plus.plus@gmail.com>
2019-10-10 18:46:55 +0300

    update schema

--- schemas/index.js
+++ schemas/index.js
@@ -1,2 +1,15 @@
+const { gql } =require( 'apollo-server-express');
 const userSchema = require('./user');
-module.exports = [userSchema]
+const messageSchema = require('./message');
+const linkSchema = gql`
+  type Query {
+    _: Boolean
+  }
+  type Mutation {
+    _: Boolean
+  }
+  type Subscription {
+    _: Boolean
+  }
+`;
+module.exports = [linkSchema,userSchema,messageSchema]
--- schemas/user.js
+++ schemas/user.js
@@ -1,25 +1,13 @@
 const { gql } = require('apollo-server-express');
 module.exports = gql`
-type Query {
+extend type Query {
   users: [User!]
   user(id: ID!): User
   me: User
-
-  messages: [Message!]!
-  message(id: ID!): Message!
 }
 type User {
   id: ID!
   username: String!
   messages: [Message!]
-}
-type Message {
-    id: ID!
-    text: String!
-    user: User!
-}
-type Mutation {
-    createMessage(text: String!): Message!
-    deleteMessage(id: ID!): Boolean!
 }
 `;
