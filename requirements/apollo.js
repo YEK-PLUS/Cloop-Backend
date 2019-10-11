f89b0d77fe5051028e51289bc82e193f08f75e5b
Yunus Emre <yek.plus.plus@gmail.com>
2019-10-12 02:06:57 +0300

    re-view context

--- requirements/apollo.js
+++ requirements/apollo.js
@@ -3,13 +3,11 @@ const schema = require('../schemas')
 const resolvers = require('../resolvers')
 const models = require('../models')
 module.exports = (app) => {
-
   const apollo = new ApolloServer({
     typeDefs: schema,
     resolvers,
     context: {
       models,
-      me: models.users[1],
     },
   });
   apollo.applyMiddleware({ app, path: '/graphql' });
