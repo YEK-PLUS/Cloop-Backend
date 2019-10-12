2dc81f974ece2db7d28bb1b3de598ab5e1ac13f4
Yunus Emre <yek.plus.plus@gmail.com>
2019-10-12 16:54:22 +0300

    updated middlewares with eslint

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,4 @@
 const { ForbiddenError } = require('apollo-server');
 const { skip } = require('graphql-resolvers');
-module.exports = (parent, args, { me }) =>
-  me ? skip : new ForbiddenError('Not authenticated as user.');
+
+module.exports = (parent, args, { me }) => (me ? skip : new ForbiddenError('Not authenticated as user.'));
