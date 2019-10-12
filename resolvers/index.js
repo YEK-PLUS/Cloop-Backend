f4c5aeb33cc10ab5c1f36c4823ed3b2650f63612
Yunus Emre <yek.plus.plus@gmail.com>
2019-10-12 04:48:46 +0300

    updated resolvers main file

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,5 +1,5 @@
 const userResolve = require('./user')
-const messageResolve = require('./message')
+const userDetail = require('./userDetail')
 const _ = require('lodash')
 
 
@@ -7,7 +7,7 @@ let Query = {};
 let Mutation = {};
 let Other = {};
 
-[userResolve,messageResolve].map((resolve)=>{
+[userResolve,userDetail].map((resolve)=>{
   Query = {...resolve.Query,...Query};
   Mutation = {...resolve.Mutation,...Mutation};
   Other = {..._.omit(resolve,['Query','Mutation']),...Other};
