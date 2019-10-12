89e807214ee52bc3a38fba9ba1f17704d66f2a23
Yunus Emre <yek.plus.plus@gmail.com>
2019-10-12 21:26:48 +0300

    update push check middleware

--- scripts/check-push.js
+++ scripts/check-push.js
@@ -1,9 +1,8 @@
-const { exec } = require('child_process')
+const { exec } = require('child_process');
 
 exec("git status | grep 'modified:' | wc -l", (err, files, outerr) => {
-  console.log('controlling files');
   if (err && outerr) { procress.exit(1); }
   if (files > 0) {
-    console.error(`\n\n\n!!!! => ${parseInt(files)} dosyayi guncelle\n\n`);
+    console.error(`\n\n\n!!!! => ${parseInt(files)} files waiting comiting\n\n`);
   }
 });
