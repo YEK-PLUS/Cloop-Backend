cf085c21368a55f32bc82449b8747e0468bd47c4
Yunus Emre <yek.plus.plus@gmail.com>
2019-10-12 14:04:40 +0300

    updated security for grapql

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,4 @@
-const hash = require('pbkdf2-password')();
-const _ = require('lodash');
-module.exports = (req) => {
-  const {username, password} = req.body || {};
-  // TODO: create database and update this lines
-  const users = {
-    YEK:{
-      username : 'YEK',
-      password: 'password'
-    }
-  };
-  if(_.has(users,username) && users[username].password === password){
-      req.session.regenerate(()=>{
-        req.session.user = users[username];
-      });
-      return true;
-  }
-  else{
-    return false;
-  }
-}
+const { ForbiddenError } = require('apollo-server');
+const { skip } = require('graphql-resolvers');
+module.exports = (parent, args, { me }) =>
+  me ? skip : new ForbiddenError('Not authenticated as user.');
