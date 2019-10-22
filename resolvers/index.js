28ba2c658b08cbb9e851c6dc2b0cb6a0957ab490
Yunus Emre <yek.plus.plus@gmail.com>
2019-10-22 21:52:43 +0300

    added new resolvers

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,13 +1,14 @@
 const _ = require('lodash');
 const userResolve = require('./user');
 const userDetail = require('./userDetail');
-
+const media = require('./media');
+const rank = require('./rank');
 
 let Query = {};
 let Mutation = {};
 let Other = {};
 
-[userResolve, userDetail].map((resolve) => {
+[userResolve, userDetail, media, rank].map((resolve) => {
   Query = { ...resolve.Query, ...Query };
   Mutation = { ...resolve.Mutation, ...Mutation };
   Other = { ..._.omit(resolve, ['Query', 'Mutation']), ...Other };
