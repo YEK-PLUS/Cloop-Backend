6d4b75732383b5192b093441e94331aeb992527d
Yunus Emre <yek.plus.plus@gmail.com>
2019-10-22 21:38:24 +0300

    update main schema file

--- schemas/index.js
+++ schemas/index.js
@@ -1,6 +1,8 @@
 const { gql } = require('apollo-server-express');
 const userSchema = require('./user');
 const userDetailSchema = require('./userDetail');
+const mediaSchema = require('./media');
+const rankSchema = require('./rank');
 
 const linkSchema = gql`
   type Query {
@@ -13,4 +15,4 @@ const linkSchema = gql`
     _: Boolean
   }
 `;
-module.exports = [linkSchema, userSchema, userDetailSchema];
+module.exports = [linkSchema, userSchema, userDetailSchema, mediaSchema, rankSchema];
