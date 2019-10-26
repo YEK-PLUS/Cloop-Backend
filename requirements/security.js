a59262e8f18b9bd7f5c9ca87cfbdb28f06f6a279
Yunus Emre <yek.plus.plus@gmail.com>
2019-10-26 22:00:20 +0300

    added no auth apollo server

--- requirements/security.js
+++ requirements/security.js
@@ -2,13 +2,15 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (app) => {
   app.use(async (req, res, next) => {
+    if(settings.whiteList.indexOf(req.path) > -1){
+      return next();
+    }
     if (req.headers.authorization) {
       try {
         const token = req.headers.authorization;
         if (await jwt.verify(token, settings.secretKeys.jwt)) {
           return next();
         }
-
         return res.status(401).send(settings.messages.sessionExpired).end();
       } catch (e) {
         return res.status(401).send(settings.messages.sessionExpired).end();
--- settings/whiteList.js
+++ settings/whiteList.js
@@ -1,4 +1,7 @@
 module.exports = [
   '/',
   '/main',
+  '/auth/login',
+  '/auth/token',
+  '/graphqlNoAuth'
 ];
