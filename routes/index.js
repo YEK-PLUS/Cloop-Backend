073f68df04cc5d1e8e7250176931324ed9ce63d3
Yunus Emre <yek.plus.plus@gmail.com>
2019-10-26 23:43:06 +0300

    added translated route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const mainRouter = require('./main');
 const authRouter = require('./auth');
+const translateRouter = require('./translate');
 
 module.exports = (app) => {
   app.use('/main', mainRouter);
   app.use('/auth', authRouter);
+  app.use('/translate', translateRouter);
 };
 module.exports.errorRoute = (app) => {
   app.use((req, res) => {
---
+++ routes/translate.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+const models = require('../models');
+const router = Router();
+router.post('/', async (req, res) => {
+  const translates = await models.TranslatedWord.findAll({
+    attributes:[`word`],
+    include:[
+      {
+        attributes:[`lang`,`translated_word`],
+        model:models.Translate,
+      }
+    ]
+  });
+
+  return res.status(200).send(translates);
+});
+
+module.exports = router;
